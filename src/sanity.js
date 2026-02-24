aa85fcab5a1a124d123840f396568cc847218838
Lumiere-Afwerking <ayoub.fethi359@gmail.com>
2026-02-24 14:52:25 +0100

    Update sanity.js

--- src/sanity.js
+++ src/sanity.js
@@ -2,15 +2,14 @@ import { createClient } from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = createClient({
-  projectId: '63ghbydx', // Jouw unieke Sanity ID
+  projectId: '63ghbydx',
   dataset: 'production',
-  useCdn: true, // Zorgt ervoor dat foto's razendsnel laden
-  apiVersion: '2024-02-24', // De datum van vandaag (of nieuwer)
+  useCdn: true,
+  apiVersion: '2024-02-24',
 });
 
 const builder = imageUrlBuilder(client);
 
-// Deze functie maakt van een Sanity afbeelding een werkende URL voor je website
 export function urlFor(source) {
   return builder.image(source);
 }
