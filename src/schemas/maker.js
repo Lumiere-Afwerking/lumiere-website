67661b48ed041da65690b682960f854cf87e45c0
Lumiere-Afwerking <ayoub.fethi359@gmail.com>
2026-02-24 14:30:24 +0100

    Create maker.js

---
+++ src/schemas/maker.js
@@ -0,0 +1,31 @@
+export default {
+  name: 'maker',
+  title: 'De Maker (Over Mij)',
+  type: 'document',
+  fields: [
+    { 
+      name: 'titel', 
+      title: 'Titel van het blok', 
+      type: 'string',
+      description: 'Bijv: Passie, Precisie & Erfgoed'
+    },
+    { 
+      name: 'verhaal1', 
+      title: 'Paragraaf 1', 
+      type: 'text',
+      description: 'Het eerste deel van je verhaal (bijv. over je Marokkaanse roots)'
+    },
+    { 
+      name: 'verhaal2', 
+      title: 'Paragraaf 2', 
+      type: 'text',
+      description: 'Het tweede deel van je verhaal (bijv. over je werkwijze in Rotterdam)'
+    },
+    { 
+      name: 'foto', 
+      title: 'Portretfoto', 
+      type: 'image', 
+      options: { hotspot: true } 
+    }
+  ]
+}
